bef4b4870425688f7aeea3cb35b1ae5c0efe6e6d
ban <bssfr123@naver.com>
2021-03-23 11:50:30 +0900

    ban

---
+++ game.py
@@ -0,0 +1,15 @@
+print("밥묵자 뭐먹을래")
+print("1. 짱깨 2. 치킨 3.피자")
+
+a = input ("ㄱ:" )
+print("ㅇㅋ ", a )
+
+if a== 1:
+    print("짱깨")
+     if a==2:
+         print("치킨")
+         if a==3:
+            print ("피자")
+             
+         
+
